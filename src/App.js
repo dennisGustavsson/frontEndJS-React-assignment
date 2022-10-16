db0d4a0ead306fdc016ba8e3be221091078e455a
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2022-10-16 22:41:48 +0200

    editing featured prods

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.min.css';
 import Navbar from './Components/navbar';
 import Showcase from './Components/showcase';
 import JumbotronOne from './Components/JumbotronOne';
+import FeaturedProducts from './Components/FeaturedProducts';
 function App() {
   return (
     <>
     <Navbar/>
     <Showcase/>
     <JumbotronOne/>
+    <FeaturedProducts/>
     </>
   );
 }
---
+++ src/Components/FeaturedProducts.js
@@ -0,0 +1,216 @@
+const FeaturedProducts = () => {
+  return (
+    <section className="featured-product container">
+    <h2 className="section-title">Featured Product</h2>
+    <div className="product-grid">
+        <div className="product-card">
+            <div className="product-img">
+                <button className="btn-theme">Quick view</button>
+            </div>
+            <div className="product-bg"></div>
+            <ul className="card-menu __nav-icons">
+                <a href=""><li class="__nav-icons"><i class="fa-regular fa-heart"></i></li></a>
+                <a href=""><li class="__nav-icons"><i class="fa-regular fa-code-compare"></i></li></a>
+                <a href=""><li class="__nav-icons"><i class="fa-regular fa-shopping-bag"></i></li></a>
+            </ul>
+            <div className="product-info">
+                <span>Category</span>
+                <a href=""><h4>Modern Black Blouse</h4></a>
+                <span className="rating">
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                </span>
+                <br/>
+                <span class="old-price">$35.00</span>
+                <span class="new-price">$30.00</span>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-img">
+                <button class="btn-theme">Quick view</button>
+            </div>
+            <div class="product-bg"></div>
+            <ul class="card-menu __nav-icons">
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-heart"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-code-compare"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-shopping-bag"></i></li></a>
+            </ul>
+            <div class="product-info">
+                <span>Category</span>
+                <a href=""><h4>Modern Black Blouse</h4></a>
+                <span class="rating">
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                </span>
+                <br/>
+                <span class="old-price">$35.00</span>
+                <span class="new-price">$30.00</span>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-img">
+                <button class="btn-theme">Quick view</button>
+            </div>
+            <div class="product-bg"></div>
+
+            <ul class="card-menu">
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-heart"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-code-compare"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-shopping-bag"></i></li></a>
+            </ul>
+            <div class="product-info">
+                <span>Category</span>
+                <a href=""><h4>Modern Black Blouse</h4></a>
+                <span class="rating">
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                </span>
+                <br/>
+                <span class="old-price">$35.00</span>
+                <span class="new-price">$30.00</span>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-img">
+                <button class="btn-theme">Quick view</button>
+            </div>
+            <div class="product-bg"></div>
+
+            <ul class="card-menu">
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-heart"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-code-compare"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-shopping-bag"></i></li></a>
+            </ul>
+            <div class="product-info">
+                <span>Category</span>
+                <a href=""><h4>Modern Black Blouse</h4></a>
+                <span class="rating">
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                </span>
+                <br/>
+                <span class="old-price">$35.00</span>
+                <span class="new-price">$30.00</span>
+            </div>
+        </div>
+                    <div class="product-card">
+            <div class="product-img">
+                <button class="btn-theme">Quick view</button>
+            </div>
+            <div class="product-bg"></div>
+
+            <ul class="card-menu">
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-heart"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-code-compare"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-shopping-bag"></i></li></a>
+            </ul>
+            <div class="product-info">
+                <span>Category</span>
+                <a href=""><h4>Modern Black Blouse</h4></a>
+                <span class="rating">
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                </span>
+                <br/>
+                <span class="old-price">$35.00</span>
+                <span class="new-price">$30.00</span>
+            </div>
+        </div>
+                    <div class="product-card">
+            <div class="product-img">
+                <button class="btn-theme">Quick view</button>
+            </div>
+            <div class="product-bg"></div>
+
+            <ul class="card-menu">
+            <a href=""><li class="nav-icons"><i class="fa-regular fa-heart"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-code-compare"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-shopping-bag"></i></li></a>
+            </ul>
+            <div class="product-info">
+                <span>Category</span>
+                <a href=""><h4>Modern Black Blouse</h4></a>
+                <span class="rating">
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                </span>
+                <br/>
+                <span class="old-price">$35.00</span>
+                <span class="new-price">$30.00</span>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-img">
+                <button class="btn-theme">Quick view</button>
+            </div>
+            <div class="product-bg"></div>
+
+            <ul class="card-menu">
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-heart"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-code-compare"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-shopping-bag"></i></li></a>
+            </ul>
+            <div class="product-info">
+                <span>Category</span>
+                <a href=""><h4>Modern Black Blouse</h4></a>
+                <span class="rating">
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                </span>
+                <br/>
+                <span class="old-price">$35.00</span>
+                <span class="new-price">$30.00</span>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-img">
+                <button class="btn-theme">Quick view</button>
+            </div>
+            <div class="product-bg"></div>
+
+            <ul class="card-menu">
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-heart"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-code-compare"></i></li></a>
+                <a href=""><li class="nav-icons"><i class="fa-regular fa-shopping-bag"></i></li></a>
+            </ul>
+            <div class="product-info">
+                <span>Category</span>
+                <a href=""><h4>Modern Black Blouse</h4></a>
+                <span class="rating">
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                    <a href=""><i class="fa-solid fa-star"></i></a>
+                </span>
+                <br/>
+                <span class="old-price">$35.00</span>
+                <span class="new-price">$30.00</span>
+            </div>
+        </div>
+    </div>
+</section>
+  )
+}
+export default FeaturedProducts
