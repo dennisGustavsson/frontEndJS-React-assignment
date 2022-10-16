e81e1be1421788e31970d723b44224598346af03
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2022-10-16 22:22:55 +0200

    added first jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.min.css';
 import Navbar from './Components/navbar';
-import Showcase from './Components/showcase'
+import Showcase from './Components/showcase';
+import JumbotronOne from './Components/JumbotronOne';
 function App() {
   return (
     <>
     <Navbar/>
     <Showcase/>
+    <JumbotronOne/>
     </>
   );
 }
---
+++ src/Components/JumbotronOne.js
@@ -0,0 +1,25 @@
+import firstJumboImg from '../Assets/images/img-5.svg';
+import secondJumboImg from '../Assets/images/img-6.svg';
+
+const JumbotronOne = () => {
+  return (
+<section className="jumbotron container">
+  <div className="jumbo-content">
+    <img src={firstJumboImg} alt=""/>
+    <div className="jumbo-text">
+      <h2>Winter Clearance Up to 70% Off!</h2>
+      <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem apriam eaque ipsa quae ab illo inventore.</span>
+      <button className="btn-theme">Shop now</button>
+    </div>
+  </div>
+  <div className="jumbo-content">
+    <img src={secondJumboImg} alt=""/>
+    <div className="jumbo-text">
+      <h2>New Arivalls</h2>
+      <button className="btn-theme">Shop now</button>
+    </div>
+  </div>
+</section>
+  )
+}
+export default JumbotronOne
