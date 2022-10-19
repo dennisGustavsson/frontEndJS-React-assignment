9fcb9acc86b7d9384e51bd3ec89990d34f25647c
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2022-10-19 17:51:09 +0200

    added service section bottom

---
+++ src/Sections/ServiceSection.js
@@ -0,0 +1,39 @@
+import customerService from '../Assets/images/icons/customer-service.svg';
+import creditCard from '../Assets/images/icons/credit-card.svg';
+import deliveryTruck from '../Assets/images/icons/delivery-truck.svg';
+const ServiceSection = () => {
+  return (
+    <section className="services">
+        <div className="container">
+            <div className="service-item">
+                <div className="img-circle"><img src={customerService} alt="customer service icon" /></div>
+
+                <h4>Customer Support</h4>
+                <span>Lorem ipsum dolor sit amet consectetur.
+                </span>
+            </div>
+            <div className="service-item">
+                <div className="img-circle">
+                <img src={creditCard} alt="customer service icon" />
+                </div>
+                <h4>Customer Support</h4>
+                <span>Lorem ipsum dolor sit amet consectetur.
+                </span>
+            </div>
+            <div className="service-item">
+                <div className="img-circle"><img src={deliveryTruck} alt="customer service icon" /></div>
+                <h4>Customer Support</h4>
+                <span>Lorem ipsum dolor sit amet consectetur.
+                </span>
+            </div>
+            <div className="service-item">
+                <div className="img-circle"><img src={deliveryTruck} alt="customer service icon" /></div>
+                <h4>Customer Support</h4>
+                <span>Lorem ipsum dolor sit amet consectetur.
+                </span>
+            </div>
+        </div>
+    </section>
+  )
+}
+export default ServiceSection
--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -4,6 +4,7 @@ import Jumbotron1Section from "../Sections/Jumbotron1Section"
 import Jumbotron2Section from "../Sections/Jumbotron2Section"
 import MainMenuSection from "../Sections/MainMenuSection"
 import OurSpecialitySection from "../Sections/OurSpecialitySection"
+import ServiceSection from "../Sections/ServiceSection"
 import ShowcaseSection from "../Sections/ShowcaseSection"
 
 const HomeView = () => {
@@ -15,6 +16,7 @@ const HomeView = () => {
     <FeaturedProductsSection/>
     <Jumbotron2Section/>
     <OurSpecialitySection/>
+    <ServiceSection/>
     <FooterSection/>
     </>
   )
